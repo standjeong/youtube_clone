31bcb3d11598f5d4ddf8ff9a6df5b0140f2a49eb
standjeong <andame9999@gmail.com>
2024-07-28 11:44:28 +0900

    feat : SearchHeader 입력값 sync

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams, Link } from 'react-router-dom';
 import { BsYoutube } from 'react-icons/bs';
 import { BsSearch } from 'react-icons/bs';
 
 export default function SearchHeader() {
   const nevigate = useNavigate();
+  const { keyword } = useParams();
   const [input, setInput] = useState('');
   const handleSubmit = (e) => {
     e.preventDefault();
     nevigate(`/videos/${input}`);
   };
+
+  useEffect(() => setInput(keyword || ''), [keyword]);
+
   return (
     <header className='w-full flex py-4 px-4'>
-      <div className='flex items-center'>
+      <Link to='/' className='flex items-center'>
         <BsYoutube className='text-brand text-3xl mr-1' />
         <h1 className='text-3xl font-medium tracking-tighter pb-1'>Youtube</h1>
-      </div>
+      </Link>
       <form className='flex w-full justify-center' onSubmit={handleSubmit}>
         <input
           className='sm:w-3/5 max-w-md outline-0 border border-r-0 rounded-l-full px-3 py-1 text-lg'
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,16 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
 import youtube from '../api/youtube';
 
 export default function Videos() {
+  const { keyword } = useParams();
   const { data: videos } = useQuery({
     queryKey: ['videos'],
     queryFn: youtube,
   });
 
-  // console.log(videos);
   return (
     <div>
-      비디오목록
+      비디오목록 {keyword}
       {videos && (
         <ul>
           {videos.map((video) => (
