2e9e62526f8aea77199ac3fb8e13670350a02737
standjeong <andame9999@gmail.com>
2024-08-05 21:36:35 +0900

    style : 로딩스피너 수정

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -3,7 +3,7 @@ import { ImSpinner2 } from 'react-icons/im';
 
 export default function LoadingSpinner() {
   return (
-    <div className='flex flex-row items-center'>
+    <div className='flex flex-row items-center justify-center'>
       <p className='px-1 text-xl opacity-90 font-thin'>Loading... </p>
       <ImSpinner2 className='animate-spin text-2xl text-sky-400' />
     </div>
